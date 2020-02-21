1263963a98c7fae47bb70f1b638147488ffbd705
15254124776 <s15863549041@126.com>
2020-02-21 10:41:28 +0800

    项目使用slf4j

--- cms-portal/src/main/java/com/cms/portal/controller/admin/LoginController.java
+++ cms-portal/src/main/java/com/cms/portal/controller/admin/LoginController.java
@@ -1,6 +1,8 @@
 package com.cms.portal.controller.admin;
 
 import com.google.code.kaptcha.Producer;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,7 @@ import java.io.IOException;
  * @date: 2020/02/16/15:17
  */
 @Controller
+@Slf4j
 public class LoginController {
 
     @Autowired
@@ -35,7 +38,7 @@ public class LoginController {
         try(ServletOutputStream outputStream = httpServletResponse.getOutputStream()){
             ImageIO.write(image,"jpg",outputStream);
         }catch (IOException e){
-            e.printStackTrace();
+            log.error("验证码生成失败");
         }
     }
 
