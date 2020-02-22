7d15e7d33bf7c53c79accd7607760334ef26604a
15254124776 <s15863549041@126.com>
2020-02-22 17:13:06 +0800

    初始化,同步github

--- cms-portal/src/main/java/com/cms/portal/controller/admin/LoginController.java
+++ cms-portal/src/main/java/com/cms/portal/controller/admin/LoginController.java
@@ -2,7 +2,6 @@ package com.cms.portal.controller.admin;
 
 import com.google.code.kaptcha.Producer;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
